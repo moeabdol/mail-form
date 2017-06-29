ec48289f5aa763f1d31960e527e61596e8a17577
moeabdol <admin.r99@gmail.com>
2017-06-29 21:15:21 +0300

    Creating our model - delivering our form

--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -37,4 +37,20 @@ class MailForm::Test < ActiveSupport::TestCase
     sample.email = ""
     assert !sample.email?
   end
+
+  setup do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test "delivers an email with attributes" do
+    ::ActionMailer::Base.delivery_method = :test
+
+    sample = SampleMail.new
+    sample.email = "user@example.com"
+    sample.deliver
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["user@example.com"], mail.from
+    assert_match "Email: user@example.com", mail.body.encoded
+  end
 end
