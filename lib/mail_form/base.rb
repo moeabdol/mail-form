89150b09e996ea6d21bd37b31ef723e46627d7c6
moeabdol <admin.r99@gmail.com>
2017-06-29 17:37:00 +0300

    Creating our model - active record compliant api

---
+++ lib/mail_form/base.rb
@@ -0,0 +1,30 @@
+module MailForm
+  class Base
+    include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    include ActiveModel::Validations
+    attribute_method_prefix "clear_"
+    attribute_method_suffix "?"
+
+    def self.attributes(*names)
+      attr_accessor(*names)
+      define_attribute_methods(names)
+    end
+
+    def persisted?
+      false
+    end
+
+    protected
+
+      def clear_attribute(attribute)
+        send("#{attribute}=", nil)
+      end
+
+      def attribute?(attribute)
+        send(attribute).present?
+      end
+  end
+end
--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
+require "fixtures/sample_mail"
 
 class MailForm::Test < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, MailForm
+  end
+
+  test "sample mail has name and email as attributes" do
+    sample = SampleMail.new
+    sample.name = "User"
+    assert_equal "User", sample.name
+    sample.email = "user@example.com"
+    assert_equal "user@example.com", sample.email
+  end
+
+  test "sample mail can clear attributes using clear_prefix" do
+    sample = SampleMail.new
+    sample.name = "User"
+    sample.email = "user@example.com"
+    assert_equal "User", sample.name
+    assert_equal "user@example.com", sample.email
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.name
+    assert_nil sample.email
+  end
+
+  test "sample mail can ask if an attribute is present or not" do
+    sample = SampleMail.new
+    assert !sample.name?
+    assert !sample.email?
+    sample.name = "User"
+    assert sample.name?
+    sample.email = "user@example.com"
+    assert sample.email?
+    sample.email = ""
+    assert !sample.email?
   end
 end
